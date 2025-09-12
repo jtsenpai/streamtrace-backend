f81950545f8db00f1154a8ab828aad672a1acf7b
Jesutoni Odufeso <ojesutoni@gmail.com>
2025-09-12 10:22:47 +0100

    Refactor models.py: Define TimeStamped and Provider models with appropriate fields and metadata

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class TimeStamped(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+
+class Provider(TimeStamped):
+    name = models.CharField(max_length=100, unique=True, db_index=True)
+    url = models.URLField(blank=True)
+    logo_url = models.URLField(blank=True)
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
